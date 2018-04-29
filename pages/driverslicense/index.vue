e3c4d3529c7aa6883b59f607fcee6565121231aa
Strangerrrrrrr <770604764@qq.com>
2018-04-29 21:06:54 +0800

    a little changes

--- pages/driverslicense/index.vue
+++ pages/driverslicense/index.vue
@@ -1,15 +1,26 @@
 <template>
   <el-card>
     <el-form label-position="left" label-width="80px">
-        <el-form-item label="用户名" prop="name">
-          <el-input id="name" name="name"></el-input>
+        <el-form-item label="姓名" prop="name">
+          <el-tag id="name" name="name">{{dlInfo.	real_name}}</el-tag>
         </el-form-item>
-        <el-form-item label="用户名" prop="name">
-          <el-input id="name" name="name" placeholder="test"></el-input> 
-          <!-- 文本提示 -->
+        <el-form-item label="身份证" prop="identity">
+          <el-tag id="name" name="name" placeholder="test">{{dlInfo.identity}}</el-tag> 
         </el-form-item>
-        <el-form-item label="用户名" prop="name">
-          <el-input id="name" name="name" v-model="dlInfo.identity"></el-input>
+        <el-form-item label="准驾车型" prop="vehicle_type">
+          <el-tag id="name" name="name" v-model="dlInfo.identity">{{dlInfo.vehicle_type}}</el-tag>
+        </el-form-item>
+        <el-form-item label="领证日期" prop="created_at">
+          <el-tag id="name" name="name">{{dlInfo.created_at}}</el-tag>
+        </el-form-item>
+        <el-form-item label="更新时间" prop="updated_at">
+          <el-tag id="name" name="name">{{dlInfo.updated_at}}</el-tag>
+        </el-form-item>
+        <el-form-item label="家庭住址" prop="address">
+          <el-input id="name" name="name" v-model="dlInfo.address"></el-input>
+           <el-button plain name="submit" @click="onSubmit">
+                  修改
+           </el-button>
         </el-form-item>
     </el-form>
   </el-card>
