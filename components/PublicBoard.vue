248fb32aa12276ccd0f217fdf5116ccf0d7c0a34
Strangerrrrrrr <770604764@qq.com>
2018-05-06 00:44:19 +0800

    publicboard

--- components/PublicBoard.vue
+++ components/PublicBoard.vue
@@ -4,18 +4,11 @@
         <span>交规宣传栏</span>
         <el-button style="float: right; padding: 3px 0" type="text"><a href=""> 更多 &gt;&gt;&gt; </a></el-button>
     </div>
-    <!-- <div class="text item" v-for="(newsItem, key) in news" :key="key">
+    <div class="text item" v-for="(item, key) in newsInfo.data" :key="key">
         <i class="fas fa-info-circle"></i>
         <a href="">
-          {{ newsItem.title }}
-          <span style="float:right;"> {{ newsItem.date }} </span>
-        </a>
-    </div> -->
-    <div class="text item" v-for="n in 10" :key="n">
-        <i class="fas fa-info-circle"></i>
-        <a href="">
-          你好呀~
-          <span style="float:right;"> 2017-08-09 </span>
+          {{ item.title }}
+          <span style="float:right;"> {{ item.created_at }} </span>
         </a>
     </div>
   </el-card>
@@ -26,20 +19,19 @@ export default {
   name: 'PublicBoard',
   data () {
     return {
-      news: [
-        {
-          link: '',
-          title: 'LRQ',
-          content: 'nihaonihao',
-          date: '2017-08-09'
-        },
-        {
-          link: '',
-          title: 'LRQ',
-          content: 'nihaonihao',
-          date: '2017-08-09'
-        }
-      ]
+      newsInfo: '',
+    }
+  },
+  mounted(){
+    this.getNewsInfo()
+  },
+  methods: {
+    getNewsInfo () {
+      let self = this
+      this.$axios.get('/api/publicboard/show')
+      .then(function(res){
+        self.newsInfo = res.data
+      })
     }
   }
 }
